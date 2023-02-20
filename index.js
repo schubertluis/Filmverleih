be3034fde9a0d2de7958c9094ded6e59f71db687
olczy <113544238+olczy@users.noreply.github.com>
2023-02-20 13:37:16 +0100

    Schritt 4 updtae

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const port = 4000;
 app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
 });
-app.get("/", (req, res) => {
-  res.json({ hello: "World!" });
-});
+// app.get("/", (req, res) => {
+//   res.json({ hello: "World!" });
+// });
 
 const films = [
   {
